1d9082f0afa9735a44a52621bbd81b748a5c4374
Hassan-paracha <dietwala78f@gmail.com>
2026-08-25 22:39:57 +0500

    Update engine.py

--- engine.py
+++ engine.py
@@ -6,9 +6,9 @@ import time
 from datetime import datetime
 
 # --- CONFIGURATION ---
-CHANNEL_HANDLE = "@HassanParacha-c3g" # Ensure this is your actual @handle
+CHANNEL_HANDLE = "@HassanParacha_c3g" # Ensure this is your actual @handle
 DB_FILE = "video_db.json"
-DAILY_INDEX_LIMIT = 180  # Updated from 180 to 18
+DAILY_INDEX_LIMIT = 380  # Updated from 380 to 180
 BATCH_SIZE = 5          
 COOLDOWN = 65           
 
